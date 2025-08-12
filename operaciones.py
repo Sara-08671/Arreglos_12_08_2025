708876271c0203267cafc0597d8088f71c139ce3
Sara NG <saritanaranjowar@gmail.com>
2025-08-12 09:57:50 -0500

    Subir al archivo
    Ejemplo de operaciones con vectores

---
+++ operaciones.py
@@ -0,0 +1,22 @@
+#Suma de vectores
+import numpy as np
+vector1 = np.array([1, 2, 3])
+vector2 = np.array([4, 5, 6])
+suma = vector1 + vector2
+print("Suma de vectores:", suma)
+
+#multiplicación de vectores
+producto = vector1 * vector2 #Multiplicación de vectores
+print("Producto de vectores:", producto)#Imprimir multiplicación
+
+#División de vectores
+división = vector1 / vector2 #División de vectores
+print("División de vectores:", división) #Imprimir division
+
+#Resta de vectores
+resta = vector1 - vector2 #resta de vectores
+print("Resta de vectores: ", resta)#Imprimir resta
+
+#Potencia de vectores
+potencia = vector1 **2 #Potencia de vectores
+print("Potencia de vectores: ", potencia)#Imprimir potencia
